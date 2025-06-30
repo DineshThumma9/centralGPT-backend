19f4b935ce057ce081ed7ce3740cb6b4f48ce293
DineshThumma9 <160909147+ductdynamic@users.noreply.github.com>
2025-06-30 23:28:42 +0530

    message7

--- src/main.py
+++ src/main.py
@@ -35,6 +35,7 @@ app.add_middleware(
         "http://localhost:55000",
 
     ],
+   allow_origin_regex="https://central-gpt-frontend.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
