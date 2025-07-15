551e61e7d13ae736324aa5ac04f2da6a05684430
Dineshthumma9 <dinesthumma15@gmail.com>
2025-07-15 13:55:26 +0530

    rgx origin

--- src/main.py
+++ src/main.py
@@ -20,24 +20,28 @@ logger.add("logs/api.log", rotation="1 MB", retention="10 days", level="INFO")
 logger.info("Server started")
 
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:55000",
         "http://localhost:55000",
+        "https://central-gpt-frontend.vercel.app",
+        "https://central-gpt-frontend-4svito8yj-ducts-projects.vercel.app",
+        "https://central-gpt-frontend-4svito8yj-ducts-projects.vercel.app",
         "http://localhost:55001",
         "http://localhost:5174",
         "http://localhost:5175",
         "http://localhost:5176",
-        "https://central-gpt-frontend.vercel.app",
-        "https://central-gpt-frontend-4svito8yj-ducts-projects.vercel.app",
+        "http://localhost:55000",
     ],
+   allow_origin_regex="https://central-gpt.*\.vercel\.app",
     allow_credentials=True,
-    allow_origin_regex=r"^https:\/\/central-gpt\.vercel\.app$",
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 import time
