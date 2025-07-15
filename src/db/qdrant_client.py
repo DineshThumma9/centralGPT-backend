d2004cfb94916151bbba9f9f51005c16ec02cfd6
Dineshthumma9 <dinesthumma15@gmail.com>
2025-07-15 08:54:28 +0530

    rdcd sze misc cnges

--- src/db/qdrant_client.py
+++ src/db/qdrant_client.py
@@ -5,6 +5,7 @@ from qdrant_client import QdrantClient
 
 qdrant_client =  QdrantClient(
             url=os.getenv("QDRANT_URL"),
+    api_key=os.getenv("QDRANT_API_KEY")
     )
 
 vector_store = QdrantVectorStore(
